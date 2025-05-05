0692841490982daa2a45b0e2af26b4ce5e436f38
Achanandhi-M <achanandhi.m@gmail.com>
2025-05-05 22:06:14 +0530

    Keploy utg test

--- Dockerfile
+++ Dockerfile
@@ -5,8 +5,8 @@ COPY . /app
 RUN go mod download
 RUN go build ./cmd/katana
 
-FROM alpine:3.20.3
+FROM alpine:3.21.3
 RUN apk add --no-cache bind-tools ca-certificates chromium
-COPY --from=builder /app/katana /usr/local/bin/
+COPY --from=build-env /app/katana /usr/local/bin/
 
 ENTRYPOINT ["katana"]
--- pkg/engine/common/base.go
+++ pkg/engine/common/base.go
@@ -208,6 +208,11 @@ func (s *Shared) Do(crawlSession *CrawlSession, doRequest DoRequestFunc) error {
 			continue
 		}
 
+		if !s.Options.ValidatePath(req.URL) {
+			gologger.Debug().Msgf("`%v` filtered path. skipping", req.URL)
+			continue
+		}
+
 		inScope, scopeErr := s.Options.ValidateScope(req.URL, crawlSession.Hostname)
 		if scopeErr != nil {
 			gologger.Debug().Msgf("Error validating scope for `%v`: %v. skipping", req.URL, scopeErr)
@@ -219,7 +224,7 @@ func (s *Shared) Do(crawlSession *CrawlSession, doRequest DoRequestFunc) error {
 		}
 
 		wg.Add()
-		// gologger.Debug().Msgf("Visting: %v", req.URL) // not sure if this is needed
+		// gologger.Debug().Msgf("Visiting: %v", req.URL) // not sure if this is needed
 		go func() {
 			defer wg.Done()
 
